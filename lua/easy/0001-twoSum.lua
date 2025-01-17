f5d843d26e7fd8b39f3569a2d581eb1584910a9f
Bryant Paz <pazbryant@proton.me>
2025-01-17 03:30:15 -0600

    chore(twoSums): leetcode easy problem solved

---
+++ lua/easy/0001-twoSum.lua
@@ -0,0 +1,14 @@
+local function twoSums(nums, target)
+	local cache = {}
+	for idx, num in ipairs(nums) do
+		local diff = target - num
+
+		if cache[diff] then
+			return { cache[diff], idx }
+		end
+
+		cache[num] = idx
+	end
+end
+
+twoSums({ 1, 3, 4, 5, 9, 33, 2, 6 }, 15)
